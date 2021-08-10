375ce1a20db1cc142e7fd86c9e859222175d5c4f
DESKTOP-M6T3JTS\Darya <svirskydarya@gmail.com>
2021-08-10 20:43:28 +0300

    init

---
+++ C21 Ex02/Board.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C21_Ex02
+{
+    class Board
+    {
+        int m_rows;
+        int m_cols;
+        static int[] m_columnFullness;
+        static int m_columnCapacityLimit = 0;
+
+        public Board(int i_ColNum, int i_RowNum)
+        {
+            m_rows = i_RowNum;
+            m_cols = i_ColNum;
+            InitFullness(i_ColNum, i_RowNum);
+        }
+        public void Init(int i_ColNum, int i_RowNum)
+        {
+            m_rows = i_RowNum;
+            m_cols = i_ColNum;
+        }
+
+        public static void InitFullness(int i_ColNum, int i_RowNum)
+        {
+            m_columnFullness = new int[i_ColNum + 1];
+            for (int i = 1; i <= i_ColNum; i++)
+            {
+                m_columnFullness[i] = 0;
+            }
+
+            m_columnCapacityLimit = i_RowNum;
+        }
+
+        public static bool AddToColumn(int i_ColIndex) // return true if success
+        {
+            bool addedSuccessfully = false;
+            if (m_columnFullness[i_ColIndex] < m_columnCapacityLimit)
+            {
+                m_columnFullness[i_ColIndex]++;
+                addedSuccessfully = true;
+            }
+
+            return addedSuccessfully;
+        }
+
+        public static bool IsCellFull(int i_ColNum, int i_RowNum)
+        {
+            return (i_RowNum > m_columnCapacityLimit - m_columnFullness[i_ColNum]);
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 1; i <= m_cols; i++)
+            {
+                Console.Write("  {0}  ", i);
+            }
+
+            for (int i = 1; i <= m_rows; i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 1; j <= m_cols; j++)
+                {
+                    Console.Write("| {0}  ", IsCellFull(j, i) ? "X" : " ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+
+                for (int j = 1; j <= m_cols; j++)
+                {
+                    Console.Write("=====");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ C21 Ex02/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C21_Ex02
+{
+    internal class Player
+    {
+        char m_Sign;
+        int m_Score;
+        bool m_IsComputer;
+
+        public Player(char i_Sign, bool i_IsComputer)
+        {
+            m_Sign = i_Sign;
+            m_Score = 0;
+            m_IsComputer = i_IsComputer;
+        }
+
+    }
+}
